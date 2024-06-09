f7a565fbf5313d3485d0114c99e83d094ab7955b
Mehmet Feyyaz Kucuk <mfkucuk555@gmail.com>
2024-06-09 05:09:20 +0300

    feat: tag, transform, sprite renderer components

--- editor/src/panels/SceneHierarchyPanel.js
+++ editor/src/panels/SceneHierarchyPanel.js
@@ -27,31 +27,73 @@ export class SceneHierarchyPanel {
         
         const tags = this.refScene.registry.get_all_with_entity(banana.ComponentType.TagComponent);
         for (const [entity, tag] of Object.entries(tags)) {
-            const flags = (this.selectedEntity == entity ? banana.ImGui.ImGuiTreeNodeFlags.Selected : 0) 
-                | banana.ImGui.ImGuiTreeNodeFlags.OpenOnArrow;
-            
-            const opened = banana.ImGui.TreeNodeEx(entity, flags, tag.getName());
-
-            if (banana.ImGui.IsItemClicked()) {
-                this.selectedEntity = entity;
-            }
-
-            if (banana.ImGui.BeginPopupContextItem()) {
-                if (banana.ImGui.MenuItem('Delete')) {
-                    this.refScene.destroyEntity(new banana.Entity(entity, this.refScene));
-                    if (this.selectedEntity == entity) {
-                        this.selectedEntity = '';
-                    }
-                }
-
-                banana.ImGui.EndPopup();
-            }
-            if (opened) {
-                banana.ImGui.TreePop();
-            }
+            this.drawEntity(entity, tag);
         }
 
         banana.ImGui.End();
+
+        banana.ImGui.Begin('Properties');
+
+        if (this.selectedEntity) {
+            this.drawComponents();
+        }
+
+        banana.ImGui.End();
+    }
+
+    drawEntity(entity, tag) {
+        const flags = (this.selectedEntity == entity ? banana.ImGui.ImGuiTreeNodeFlags.Selected : 0) 
+        | banana.ImGui.ImGuiTreeNodeFlags.OpenOnArrow;
+    
+        const opened = banana.ImGui.TreeNodeEx(entity, flags, tag.getName());
+
+        if (banana.ImGui.IsItemClicked()) {
+            this.selectedEntity = entity;
+        }
+
+        if (banana.ImGui.BeginPopupContextItem()) {
+            if (banana.ImGui.MenuItem('Delete')) {
+                this.refScene.destroyEntity(new banana.Entity(entity, this.refScene));
+                if (this.selectedEntity == entity) {
+                    this.selectedEntity = '';
+                }
+            }
+
+            banana.ImGui.EndPopup();
+        }
+        if (opened) {
+            banana.ImGui.TreePop();
+        }
+    }
+
+    drawComponents() {
+        // Tag Component
+        const tag = this.refScene.registry.get(this.selectedEntity, banana.ComponentType.TagComponent);
+        let newTag = ''
+        if (banana.ImGui.InputText('Name', (value = tag.getName()) => newTag = value)) {
+            tag.setName(newTag);
+        }
+
+        // Transform Component
+        if (banana.ImGui.TreeNodeEx('TransformComponent', banana.ImGui.ImGuiTreeNodeFlags.DefaultOpen, 'Transform')) {
+            const transform = this.refScene.registry.get(this.selectedEntity, banana.ComponentType.TransformComponent);
+    
+            banana.ImGui.DragFloat3('Position', transform.getPosition(), 0.1);
+            banana.ImGui.DragFloat3('Rotation', transform.getRotation(), 0.1);
+            banana.ImGui.DragFloat3('Scale', transform.getScale(), 0.1);
+
+            banana.ImGui.TreePop();
+        }
+
+        // Sprite Renderer Component
+        if (this.refScene.registry.has(this.selectedEntity, banana.ComponentType.SpriteRendererComponent)) {
+            if (banana.ImGui.TreeNodeEx('SpriteRendererComponent', banana.ImGui.ImGuiTreeNodeFlags.DefaultOpen, 'Sprite Renderer')) {
+                const sprite = this.refScene.registry.get(this.selectedEntity, banana.ComponentType.SpriteRendererComponent);
+
+                banana.ImGui.ColorEdit4('Color', sprite.getColor());
+            }
+        }
+
     }
 
     setRefScene(scene) {
